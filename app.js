4a9c8a87a4fba897255139e13f6fc57c99831eb5
spoudel2 <47367302+spoudel2@users.noreply.github.com>
2019-07-31 11:52:06 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get('/', function (req, res) {
   res.send('Nodejs for github ci cd');
 });
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 4000);
 
 module.exports = app;
